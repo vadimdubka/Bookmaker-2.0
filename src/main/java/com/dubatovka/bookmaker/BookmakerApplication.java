bf270be363122ab9b87a4edffbb1d30599fcf2b8
vadimdubka <vadimdubka@gmail.com>
2018-06-20 20:20:00 +0300

    initial commit

---
+++ src/main/java/com/dubatovka/bookmaker/BookmakerApplication.java
@@ -0,0 +1,12 @@
+package com.dubatovka.bookmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmakerApplication.class, args);
+	}
+}
