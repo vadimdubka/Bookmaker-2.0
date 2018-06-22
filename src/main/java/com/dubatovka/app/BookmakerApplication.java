ee3fda706482c45fa5383412c3ed5e9940243c71
vdrex <vadim.dubatovka@itrexgroup.com>
2018-06-22 15:48:04 +0300

    BookmakerApplication class is ready.

--- src/main/java/com/dubatovka/app/BookmakerApplication.java
+++ src/main/java/com/dubatovka/app/BookmakerApplication.java
@@ -2,11 +2,21 @@ package com.dubatovka.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class BookmakerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookmakerApplication.class, args);
-	}
+@SpringBootApplication // Shortcut to @Configuration, @EnableAutoConfiguration and @ComponentScan
+@EnableJpaRepositories // Search and enable all interfaces with @Repository annotation
+/*Detect all classes with WebFilter, WebListener and WebServlet annotations.*/
+@ServletComponentScan(basePackages = {
+    "com.dubatovka.app.controller",
+    "com.dubatovka.app.filter",
+    "com.dubatovka.app.listener"
+})
+public class BookmakerApplication extends SpringBootServletInitializer {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BookmakerApplication.class, args);
+    }
 }
