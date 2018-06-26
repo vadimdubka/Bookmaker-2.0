321e4f7d5a2b5230eec24af20ed6bdc53177feb5
vdrex <vadim.dubatovka@itrexgroup.com>
2018-06-26 16:46:51 +0300

    ChangeLocale command is replaced with controller.

--- src/main/java/com/dubatovka/app/controller/impl/ChangeLocaleCommand.java
+++ src/main/java/com/dubatovka/app/controller/impl/ChangeLocaleCommand.java
@@ -3,6 +3,9 @@ package com.dubatovka.app.controller.impl;
 import com.dubatovka.app.config.ConfigConstant;
 import com.dubatovka.app.controller.Command;
 import com.dubatovka.app.controller.PageNavigator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,7 +19,21 @@ import static com.dubatovka.app.config.ConfigConstant.PARAM_LOCALE;
  *
  * @author Dubatovka Vadim
  */
+@Controller
+//TODO переименовать команды в контроллер
 public class ChangeLocaleCommand implements Command {
+    
+    @GetMapping("/change_locale")
+    public String showMainPage(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String locale = request.getParameter(PARAM_LOCALE);
+        if (locale == null) {
+            locale = LOCALE_DEFAULT;
+        }
+        session.setAttribute(ATTR_LOCALE, locale);
+        return "redirect:/main"; //TODO сделать так, чтобы форвардило на предыдущую запрошенную страницу, а не всегда на main
+    }
+    
     /**
      * Takes {@link ConfigConstant#PARAM_LOCALE} parameter from {@link HttpServletRequest , sets it
      * as attribute to {@link HttpSession } and navigates to {@link PageNavigator#FORWARD_PREV_QUERY}.
@@ -24,10 +41,11 @@ public class ChangeLocaleCommand implements Command {
      * @param request {@link HttpServletRequest } from client
      * @return {@link PageNavigator#FORWARD_PREV_QUERY}
      */
+    @Deprecated
     @Override
     public PageNavigator execute(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        String      locale  = request.getParameter(PARAM_LOCALE);
+        String locale = request.getParameter(PARAM_LOCALE);
         if (locale == null) {
             locale = LOCALE_DEFAULT;
         }
