9bd285f406d4db2d0ddec6361267866ebca97253
vdrex <vadim.dubatovka@itrexgroup.com>
2018-06-26 16:26:38 +0300

    Commands for main and index pages are replaced with controller.

--- src/main/java/com/dubatovka/app/controller/impl/navigation/GotoIndexCommand.java
+++ src/main/java/com/dubatovka/app/controller/impl/navigation/GotoIndexCommand.java
@@ -3,7 +3,6 @@ package com.dubatovka.app.controller.impl.navigation;
 import com.dubatovka.app.controller.Command;
 import com.dubatovka.app.controller.PageNavigator;
 import com.dubatovka.app.service.QueryService;
-import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,18 +12,13 @@ import javax.servlet.http.HttpServletRequest;
  * @author Dubatovka Vadim
  */
 @Deprecated
-@Controller
 public class GotoIndexCommand implements Command {
-    
-    
-    
     /**
      * Method provide navigation process to index page.
      *
      * @param request {@link HttpServletRequest} from client.
      * @return {@link PageNavigator#FORWARD_PAGE_INDEX}.
      */
-    
     @Override
     public PageNavigator execute(HttpServletRequest request) {
         QueryService.saveQueryToSession(request);
